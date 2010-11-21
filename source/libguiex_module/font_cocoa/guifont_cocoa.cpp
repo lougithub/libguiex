fcb1918dd950b3843779c14b6593556cfa803bc3
Guoliang Lou <louguoliang@gmail.com>
2010-11-21 15:40:22 +0000

    [TASK] success to run scene in iphone.

--- source/libguiex_module/font_cocoa/guifont_cocoa.cpp
+++ source/libguiex_module/font_cocoa/guifont_cocoa.cpp
@@ -1,9 +1,9 @@
 /** 
-* @file guirender_cocoa.cpp
-* @brief use freetype2 as font engine
-* @author ken
-* @date 2010-11-10
-*/
+ * @file guirender_cocoa.cpp
+ * @brief use freetype2 as font engine
+ * @author ken
+ * @date 2010-11-10
+ */
 
 
 
@@ -72,37 +72,133 @@ namespace guiex
 	}
 	//------------------------------------------------------------------------------
 	void IGUIFont_cocoa::DrawCharacter(
-		IGUIInterfaceRender* pRender, 
-		const CGUIMatrix4& rWorldMatrix,
-		wchar_t charCode, 
-		const CGUIStringInfo& rInfo,
-		const CGUIVector2& rPos,
-		real fAlpha)
+									   IGUIInterfaceRender* pRender, 
+									   const CGUIMatrix4& rWorldMatrix,
+									   wchar_t charCode, 
+									   const CGUIStringInfo& rInfo,
+									   const CGUIVector2& rPos,
+									   real fAlpha)
 	{
-	
+		CGUIRect aCharRect( CGUIVector2(rPos.x, rPos.y),
+						   CGUISize(rInfo.m_nFontSize,rInfo.m_nFontSize));
+		
+		pRender->DrawRect( rWorldMatrix, aCharRect, 1, pRender->GetAndIncZ(),
+						  rInfo.m_aColor.GetARGB(),
+						  rInfo.m_aColor.GetARGB(),
+						  rInfo.m_aColor.GetARGB(),
+						  rInfo.m_aColor.GetARGB());
 	}
 	//------------------------------------------------------------------------------
 	void IGUIFont_cocoa::DrawString(IGUIInterfaceRender* pRender, 
-		const CGUIMatrix4& rWorldMatrix,
-		const CGUIStringEx& rString, 
-			const CGUIRect&	rStringRect,
-			const uint8&	uTextAlignment,
-			real			fAlpha,
-			int32 nStartPos,
-			int32 nEndPos)
+									const CGUIMatrix4& rWorldMatrix,
+									const CGUIStringEx& rString, 
+									const CGUIRect&	rStringRect,
+									const uint8&	uTextAlignment,
+									real			fAlpha,
+									int32 nStartPos,
+									int32 nEndPos)
 	{
-	
+		if( rString.m_strContent.empty())
+		{
+			//empty string
+			return;
+		}
+		
+		CGUIVector2 aPos;
+		
+		real fScaledStringWidth = GetStringWidth(rString);
+		real fScaledStringHeight = rString.GetStringInfo().m_nFontSize;
+		
+		if(( uTextAlignment & GUI_TA_HORIZON_MASK) == eTextAlignment_Horz_Left )
+		{
+			//for horizon left
+			aPos.x = rStringRect.m_fLeft;
+		}
+		else if(( uTextAlignment & GUI_TA_HORIZON_MASK) == eTextAlignment_Horz_Right )
+		{
+			//for horizon right
+			aPos.x = rStringRect.m_fRight-fScaledStringWidth;
+		}
+		else
+		{
+			//for horizon center
+			aPos.x = rStringRect.m_fLeft+(rStringRect.GetWidth()-fScaledStringWidth)/2;
+		}
+		
+		if(( uTextAlignment & GUI_TA_VERTICAL_MASK) == eTextAlignment_Vert_Up )
+		{
+			//for vertical up
+			aPos.y = rStringRect.m_fTop;
+		}
+		else if(( uTextAlignment & GUI_TA_VERTICAL_MASK) == eTextAlignment_Vert_Down )
+		{
+			//for vertical down
+			aPos.y = rStringRect.m_fBottom - fScaledStringHeight;
+		}
+		else
+		{
+			//for vertical center
+			aPos.y = rStringRect.m_fTop + (rStringRect.GetHeight() - fScaledStringHeight) / 2;
+		}
+		if( nEndPos < 0 || nEndPos >int32( rString.m_strContent.size()))
+		{
+			nEndPos = rString.m_strContent.size();
+		}
+		
+		const CGUIStringInfo& rInfo = rString.m_aStringInfo;
+		for( int32 i= nStartPos; i<nEndPos; ++i)
+		{
+			
+			CGUIRect aCharRect(
+							   CGUIVector2(aPos.x, aPos.y+real(rInfo.m_nFontSize)),
+							   CGUISize(rInfo.m_nFontSize,rInfo.m_nFontSize));
+			
+			//dest area size
+			pRender->DrawRect( rWorldMatrix, aCharRect, 1, pRender->GetAndIncZ(),
+							  rInfo.m_aColor.GetARGB(),
+							  rInfo.m_aColor.GetARGB(),
+							  rInfo.m_aColor.GetARGB(),
+							  rInfo.m_aColor.GetARGB());
+			
+			aPos.x+=rInfo.m_nFontSize;
+		}
 	}
 	//------------------------------------------------------------------------------
 	void IGUIFont_cocoa::DrawString(IGUIInterfaceRender* pRender, 
-		const CGUIMatrix4& rWorldMatrix,
-		const CGUIStringEx& rString, 
-		const CGUIVector2& rPos,
-		real fAlpha,
-		int32 nStartPos,
-		int32 nEndPos)
+									const CGUIMatrix4& rWorldMatrix,
+									const CGUIStringEx& rString, 
+									const CGUIVector2& rPos,
+									real fAlpha,
+									int32 nStartPos,
+									int32 nEndPos)
 	{
-
+		if( rString.m_strContent.empty())
+		{
+			//empty string
+			return;
+		}
+		CGUIVector2 aPos = rPos;
+		
+		if( nEndPos<0 || nEndPos>int32(rString.m_strContent.size()))
+		{
+			nEndPos = rString.m_strContent.size();
+		}
+		
+		const CGUIStringInfo& rInfo = rString.m_aStringInfo;
+		for( int32 i=nStartPos; i<nEndPos; ++i)
+		{
+			CGUIRect aCharRect(
+							   CGUIVector2(aPos.x, aPos.y+rInfo.m_nFontSize),
+							   CGUISize(rInfo.m_nFontSize,rInfo.m_nFontSize));
+			
+			pRender->DrawRect( rWorldMatrix, aCharRect, 1, pRender->GetAndIncZ(),
+							  rInfo.m_aColor.GetARGB(),
+							  rInfo.m_aColor.GetARGB(),
+							  rInfo.m_aColor.GetARGB(),
+							  rInfo.m_aColor.GetARGB());
+			
+			aPos.x+=rInfo.m_nFontSize;
+		}
 	}
 	//------------------------------------------------------------------------------
 	const CGUISize& IGUIFont_cocoa::GetCharacterSize(int32 nFontFaceIdx,wchar_t charCode,uint32 nSize)
