27ddba27be5748729c8b5c25fe531a54e283cc0b
Guoliang Lou <louguoliang@gmail.com>
2011-04-29 10:20:50 +0000

    [TASK] build in android (WIP)

--- test/test1/android/jni/Android.mk
+++ test/test1/android/jni/Android.mk
@@ -2,10 +2,12 @@ LOCAL_PATH := $(call my-dir)
 GUIEX_Root := $(LOCAL_PATH)/../../../..
 
 include $(CLEAR_VARS)
-
 LOCAL_MODULE := test1
 
-LOCAL_SRC_FILES := ../../test1.cpp
+
+LOCAL_PREBUILT_LIBS := $(GUIEX_Root)/android/obj/local/armeabi/libguiex_core.a
+include $(BUILD_MULTI_PREBUILT)
+LOCAL_STATIC_LIBRARIES += libguiex_core
 
 LOCAL_C_INCLUDES := \
 	$(GUIEX_Root)/ \
@@ -17,20 +19,23 @@ LOCAL_C_INCLUDES := \
 	$(GUIEX_Root)/external/libogg/include \
 	$(GUIEX_Root)/external/freealut/include 
 
+##LOCAL_STATIC_LIBRARIES
+#LOCAL_LDLIBS := \
+	#-l$(GUIEX_Root)/android/obj/local/armeabi/libguiex_core.a \
+	#-l$(GUIEX_Root)/android/obj/local/armeabi/libguiex_framework.a
+	##libguiex_core \
+	#libguiex_widget \
+	#libguiex_widget_game \
+	#libguiex_widget_box2d \
+	#libguiex_framework \
+	#liblua \
+	#libbox2d \
+	#libogg \
 
 LOCAL_LDLIBS := \
-	-L$(GUIEX_Root)/android/obj/local/armeabi \
-	libguiex_core.a \
-	-lguiex_widget \
-	-lguiex_widget_box2d \
-	-lguiex_widget_game \
-	-lguiex_framework \
-	-llua \
-	-lbox2d \
-	-logg \
-	-llog \
-	-lEGL
+	-llog 
 
+LOCAL_SRC_FILES := ../../test1.cpp
 
 include $(BUILD_SHARED_LIBRARY)
 
