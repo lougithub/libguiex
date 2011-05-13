04e1044512109e5f8a6de94f14ac78d9b22c7e9e
Guoliang Lou <louguoliang@gmail.com>
2011-05-13 08:30:28 +0000

    [TASK] update android version.

---
+++ test/_android_project_/jni/testparticle2d.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH:=$(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE:=guiex_sample
+LOCAL_SRC_FILES := ../../common_android/common_android.cpp \
+	../../testparticle2d/testparticle2d.cpp
+
+
+
+include $(LOCAL_PATH)/android_library.mk
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(LOCAL_PATH)/android_import_module.mk
---
+++ test/_android_project_/src/com/android/GuiexSample/GuiexSampleView.java
@@ -0,0 +1,83 @@
+package com.android.GuiexSample;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.opengl.GLSurfaceView;
+import android.util.Log;
+import android.view.MotionEvent;
+
+class GuiexSampleView extends GLSurfaceView 
+{
+    public GuiexSampleView(Context context) 
+	{
+        super(context);
+        			
+        init( context );
+    }
+
+    private void init( Context context ) 
+	{
+    	setEGLConfigChooser(5, 6, 5, 0, 0, 0); 
+    	
+		String apkFilePath = null;
+		ApplicationInfo appInfo = null;
+		PackageManager packMgmr = context.getPackageManager();
+        
+		try 
+		{
+		    appInfo = packMgmr.getApplicationInfo("com.android.GuiexSample", 0);
+	    } 
+		catch (NameNotFoundException e)
+		{
+		    e.printStackTrace();
+		    throw new RuntimeException("Unable to locate assets, aborting...");
+	    }
+		apkFilePath = appInfo.sourceDir;
+	    GuiexSampleLib.SetApkPath( apkFilePath ); 
+			
+        /* Set the renderer responsible for frame rendering */
+        setRenderer(new Renderer());
+    }
+
+    private static class Renderer implements GLSurfaceView.Renderer
+    {
+        public void onDrawFrame(GL10 gl) 
+		{
+        	//Log.i("GuiexSampleView", "onDrawFrame");
+        	 GuiexSampleLib.Step();
+        }
+
+        public void onSurfaceChanged(GL10 gl, int width, int height) 
+		{
+        	Log.i("GuiexSampleView", "onSurfaceChanged");
+            GuiexSampleLib.Init(width, height); 
+        }
+
+        public void onSurfaceCreated(GL10 gl, EGLConfig config) 
+		{
+            // Do nothing.
+        }
+    }
+    
+	@Override public boolean dispatchTouchEvent(MotionEvent ev) 
+	{
+        switch( ev.getAction())
+        {
+        case  MotionEvent.ACTION_DOWN:
+        	GuiexSampleLib.OnFingerDown(ev.getX(), ev.getY());
+        	break;
+        case  MotionEvent.ACTION_UP:
+        	GuiexSampleLib.OnFingerUp(ev.getX(), ev.getY());
+        	break;
+        case  MotionEvent.ACTION_MOVE:
+        	GuiexSampleLib.OnFingerMove(ev.getX(), ev.getY());
+        	break;
+        }
+        return true;
+    }
+}
